c9f4f038f598d81768ff42ed32a1147966b575ac
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2024-06-05 20:56:43 +0100

    Add funtion getRandomColor: math.random

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
  
- 
- 
+ let controls = document.querySelector(".controls");
+ let partAControls = document.querySelector(".partAControls");
 const eraser = document.querySelector('button.eraser')
-  const etch = document.querySelector(".etch");
+  let etch = document.querySelector(".etch");
   
-  
+  // div creation from prompt input
  box = parseInt(prompt("How many squares do you want to build?\n Choose a number from 30 to 100")
  );
+
+
+
   //box = box ** 2;
   
       for(let i = 1; i <= box ** 2; i++){
@@ -18,52 +21,76 @@ const eraser = document.querySelector('button.eraser')
         div.style.backgroundColor = "blue";
         div.style.border = "solid";
         etch.appendChild(div);
-      };
+        div.setAttribute('id',`draw${i}`);
+      // div.addEventListener('click', alert("you press me"));
+    };
+
+
 
     // event delegation for partAControls buttons
-      let partAControls = document.querySelector(".partAControls");
-      partAControls.addEventListener("click",(e)=>{
-        let target = e.target;
-        switch(target.id){
-          case 'eraser':
-            etch.last
-            console.log("eraser");
-            break;
-            case 'black':
-              console.log("black");
-              break;
-              case 'hue':
-                console.log("hue");
-                break;  
-              
-        }
-      })
+    let draw = document.querySelector(".draw")  
 
-      // event delegation for Controls buttons
-    let controls = document.querySelector(".controls");
-    controls.addEventListener("click",(e)=>{
+    partAControls.addEventListener("click",(e)=>{
       let target = e.target;
       switch(target.id){
-        case 'left':
-          etch.last
-          console.log("left");
+        case 'eraser': etch.onmouseover(
+          draw.style.backgroundColor = "white")
+          console.log("eraser");
           break;
-
-          case 'right':
-            console.log("right");
+          case 'black':etch.onmouseover(
+            draw.style.backgroundColor = "black")
+            console.log("black");
             break;
-
-            case 'reset':
-              console.log("reset");
-              break; 
-
-              case 'up':
-              console.log("up");
-              break; 
-              
-              case 'down':
-              console.log("down");
+            case 'hue':
+              console.log("hue");
               break;  
-      
+            
       }
     })
+
+    // event delegation for Controls buttons
+  controls.addEventListener("click",(e)=>{
+    let target = e.target;
+    switch(target.id){
+      case 'left':
+        key.left
+        console.log("left");
+        break;
+
+        case 'right':
+          console.log("right");
+          break;
+
+          case 'reset':
+            console.log("reset");
+            break; 
+
+            case 'up':
+            console.log("up");
+            keyup
+            break; 
+            
+            case 'down':
+            console.log("down");
+            break;  
+    
+    }
+  })
+
+  etch.addEventListener("mouseover", (e) => {
+    let target = e.target.id;
+    target== getRandomColor();
+    return target;
+  
+    })
+   
+
+    function getRandomColor(){
+     let num = '123456789ABCDEF'
+     let color = '#'
+
+     for (let i= 0; i < 6; i++){
+      color += num[Math.floor(Math.random()*16)];
+     }
+      target.style.backgroundColor = color;
+    }getRandomColor();
