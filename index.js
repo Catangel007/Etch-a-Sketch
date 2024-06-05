996eea315af9b933f5de013b8a73e3f7b8ae5143
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2024-06-05 12:59:44 +0100

    feat flexbox: create divsin row

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
  
  
+ 
 
+  const etch = document.querySelector(".etch");
+  let square= document.querySelector("div.square");
   
+ box = parseInt(prompt("How many squares do you want to build?\n Choose a number from 10 to 100")
+ );
+  box = box ** 2;
   
+      for(let i = 1; i <= box; i++){
+         console.log(i);
+         console.log(box) ;
+        let div = document.createElement('div');
+        div.style.height="10px";
+        div.style.width= "10px";
+        div.style.backgroundColor ="blue";
+        div.style.border ="solid";
+        etch.appendChild(div);
+
+      };
+    
+      
