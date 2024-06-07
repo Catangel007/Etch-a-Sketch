0aaa56d467893159acd46329621b92ba75d4953b
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2024-06-07 16:21:11 +0100

    add functionality: eraser,hue,black

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
  
  let controls = document.querySelector(".controls");
  let partAControls = document.querySelector(".partAControls");
-const eraser = document.querySelector('button.eraser')
+const eraser = document.querySelector('#eraser')
   let etch = document.querySelector(".etch");
   
   // div creation from prompt input
  box = parseInt(prompt("How many squares do you want to build?\n Choose a number from 30 to 100")
  );
-
-
-
-  //box = box ** 2;
+ let hex = '';
   
       for(let i = 1; i <= box ** 2; i++){
          console.log(i);
@@ -18,64 +15,98 @@ const eraser = document.querySelector('button.eraser')
         let div = document.createElement('div');
         div.style.height ="10px";
         div.style.width = "10px";
-        div.style.backgroundColor = sketch;
+        //div.style.backgroundColor = getRandomColor();
         div.style.border = "solid";
         etch.appendChild(div);
         div.setAttribute('id',`draw${i}`);
-      // div.addEventListener('click', alert("you press me"));
-    };
-
-
-
-    // event delegation for partAControls buttons
-    let draw = document.querySelector(".draw")  
-
+        
+    etch.addEventListener("mouseover", (e) => {
+       hex = e.target;
+        switch(hex.id){
+          case `draw${i}`:
+          div.style.backgroundColor = getRandomColor();
+          break;
+        } 
+      }) 
+      
+        // event delegation for partAControls buttons
     partAControls.addEventListener("click",(e)=>{
       let target = e.target;
       switch(target.id){
-        case 'eraser': etch.onmouseover(
-          draw.style.backgroundColor = "white")
-          console.log("eraser");
-          break;
-          case 'black':etch.onmouseover(
-            draw.style.backgroundColor = "black")
-            console.log("black");
-            break;
+
+        case 'eraser': 
+        etch.addEventListener("mouseover", (e) => {
+          hex = e.target;
+           switch(hex.id){
+             case `draw${i}`:
+             div.style.backgroundColor = "white";  
+           } 
+         }) 
+             break;
+          
+          case 'black':
+            etch.addEventListener("mouseover", (e) => {
+            hex = e.target;
+             switch(hex.id){
+               case `draw${i}`:
+               div.style.backgroundColor = "black";  
+             } 
+           }) 
+                 break;
+          
             case 'hue':
-              console.log("hue");
-              break;  
-            
+              etch.addEventListener("mouseover", (e) => {
+                hex = e.target;
+                 switch(hex.id){
+                   case `draw${i}`:
+                   div.style.backgroundColor = getRandomColor();
+                   
+                 } 
+               }) 
+                break;     
       }
-    })
+    }) 
+       
 
-    // event delegation for Controls buttons
-  controls.addEventListener("click",(e)=>{
-    let target = e.target;
-    switch(target.id){
-      case 'left':
-        key.left
-        console.log("left");
-        break;
 
-        case 'right':
-          console.log("right");
-          break;
 
+        const moveAmount = 10;
+        let x = 0;
+        let y = 0;
+   // event delegation for Controls buttons
+     controls.addEventListener( "click", (e) => {
+        let trace = e.target;
+        switch(trace.id){
           case 'reset':
+            div.style.backgroundColor = 'white';
             console.log("reset");
             break; 
 
-            case 'up':
-            console.log("up");
-            keyup
-            break; 
-            
-            case 'down':
-            console.log("down");
-            break;  
-    
+          case "left":
+            etch.addEventListener("click", (e) => {
+              hex = e.target;
+               switch(hex.id){
+                 case `draw${i}`:
+                key[ArrowLeft]; ;
+                x-=moveAmount;
+                 div.style.backgroundColor = "black";
+                div.style.width = "12px";
+               } 
+             }) 
+              break;     
+
+        }
+     })
+
+
     }
-  })
+  
+
+
+     
+   
+
+  
     
   function getRandomColor(){
     let num = '123456789ABCDEF';
@@ -84,18 +115,8 @@ const eraser = document.querySelector('button.eraser')
     for (let i= 1; i < 7; i++){
      color += num[Math.floor(Math.random()*15)];
      
-    } 
-    
-  }
+    }
+    return color;
+     
+  }getRandomColor()
   
-
-  function sketch (){
-    let hex = '';
-  etch.addEventListener("mouseover", (e) => {
-     hex = e.target.id;
-     console.log(hex);
-    hex += getRandomColor()
-    })   
-  }
-  sketch()
-  console.log(sketch());
